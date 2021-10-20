9c6ba3ec53c4cf9888fc80b7e3928fb86c523f3b
Nataniel Porto <natanielkporto@gmail.com>
2021-10-20 20:05:54 -0300

    Separates the responsibility of the beer listing to another file

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,12 @@
 import { useEffect, useState } from 'react';
-
-const data = [
-  {
-    id: '1',
-    name: 'Pilsner',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-  {
-    id: '2',
-    name: 'IPA',
-    minimumTemperature: 5,
-    maximumTemperature: 6,
-  },
-  {
-    id: '3',
-    name: 'Lager',
-    minimumTemperature: 4,
-    maximumTemperature: 7,
-  },
-  {
-    id: '4',
-    name: 'Stout',
-    minimumTemperature: 6,
-    maximumTemperature: 8,
-  },
-  {
-    id: '5',
-    name: 'Wheat beer',
-    minimumTemperature: 3,
-    maximumTemperature: 5,
-  },
-  {
-    id: '6',
-    name: 'Pale Ale',
-    minimumTemperature: 4,
-    maximumTemperature: 6,
-  },
-];
+import beerCollection from './beerCollection';
 
 function App() {
   const [items, setItems] = useState({});
 
   useEffect(() => {
     const request = () =>
-      data.forEach((product) => {
+      beerCollection.forEach((product) => {
         fetch(`http://localhost:8081/temperature/${product.id}`)
           .then((response) => response.json())
           .then((response) =>
